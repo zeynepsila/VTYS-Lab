38ed4615e71e6da7bf084adfd6bbdf592c7342d8
Zeynep Sıla Kaymak <72812386+zeynepsila@users.noreply.github.com>
2024-07-23 00:09:34 +0300

    Add files via upload

---
+++ Foy5/arayüz/labfoy5/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using labfoy5.Models;
+using YourNamespace.Models;
+
+namespace labfoy5.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Ogrenci> Ogrenciler { get; set; }
+        public DbSet<Bolum> Bolumler { get; set; }
+        public DbSet<Fakulte> Fakulteler { get; set; }
+        public DbSet<OgrenciDers> OgrenciDersler { get; set; }
+    }
+}
